344349c33aa8614e536337fad76fb71e1e29c137
vsarapin-dev <vsarapin.dev@gmail.com>
2024-06-25 17:47:07 +0300

    vendors become

--- queries.go
+++ queries.go
@@ -295,3 +295,74 @@ func GetOperatorsBecomesCount(dbConfig DBConfig, progress *pb.ProgressBar) map[s
 	progress.Increment()
 	return result
 }
+
+func GetVendorsBecomesCount(dbConfig DBConfig, progress *pb.ProgressBar) map[string][4]uint {
+	db, err := GetDBConnection(dbConfig)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`
+		WITH RECURSIVE date_series AS (
+			SELECT '2024-05-20' AS created_date
+			UNION ALL
+			SELECT created_date + INTERVAL 1 DAY
+			FROM date_series
+			WHERE created_date <= '2024-06-21'
+		), hour_series AS (
+			SELECT 0 AS hour_start, 6 AS hour_end
+			UNION ALL
+			SELECT 6 AS hour_start, 12 AS hour_end
+			UNION ALL
+			SELECT 12 AS hour_start, 18 AS hour_end
+			UNION ALL
+			SELECT 18 AS hour_start, 24 AS hour_end
+		)
+		SELECT
+			COALESCE(COUNT(DISTINCT akb.id), 0) AS agent_count,
+			date_series.created_date,
+			hour_series.hour_start,
+			hour_series.hour_end
+		FROM
+			date_series
+		CROSS JOIN
+			hour_series
+		LEFT JOIN
+			agent_kyc_becomes akb ON DATE_FORMAT(akb.created_at, '%Y-%m-%d') = date_series.created_date
+						AND akb.interesting_account = 'Vendor'
+						AND akb.created_at >= '2024-05-20'
+						AND akb.created_at <= '2024-06-21'
+						AND HOUR(akb.created_at) >= hour_series.hour_start
+                    	AND HOUR(akb.created_at) < hour_series.hour_end
+		GROUP BY
+			date_series.created_date, hour_series.hour_start, hour_series.hour_end
+		ORDER BY
+			date_series.created_date, hour_series.hour_start;
+	`)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	result := make(map[string][4]uint)
+
+	for rows.Next() {
+		var agentCount uint
+		var createdAtStr string
+		var hourStart, hourEnd int
+
+		err = rows.Scan(&agentCount, &createdAtStr, &hourStart, &hourEnd)
+		if err != nil {
+			panic(err)
+		}
+
+		dateKey := createdAtStr
+		tempArray := result[dateKey]
+		index := (hourStart / 6) % 4
+		tempArray[index] += agentCount
+		result[dateKey] = tempArray
+	}
+	progress.Increment()
+	return result
+}
